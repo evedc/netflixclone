77aefb432a38b04ce3dd90167ed53df989bc5765
Évelim Dornelles <dorrrnelles@gmail.com>
2024-09-21 15:04:42 -0300

    pré-finalizacao

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 $('.pergunta').click(function () {
-    $('.resposta').slideToggle();
-});
+    //PEGA O ATRIBUTO DATA-PERGUNTA
+    var id = $(this).attr('data-pergunta');
+
+    //VARRE TODAS AS RESPOSTAS PARA VER SE É
+    //DAQUELA PERGUNTA CLICADA
+    $('.resposta').each(function () {
+        if ($(this).attr('id') == id) {
+            $(this).slideToggle('fast');
+        } else {
+            $(this).slideUp('fast');
+        }
+    });
+
+    //TROCAR ICONE
+    $('.icon').each(function () {
+        var valor = $(this).text();
+        if ($(this).attr('id') == "icone-" + id) {
+
+            if (valor == '+') {
+                $(this).text('X');
+            } else {
+                $(this).text('+');
+            }
+
+        } else {
+            $(this).text('+');
+        }
+    });
+
+
+})
